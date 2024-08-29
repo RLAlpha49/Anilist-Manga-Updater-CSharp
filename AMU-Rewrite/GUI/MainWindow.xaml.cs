e6802d75413711519d718faab505d1ba2c2f8bcb
RLAlpha49 <75044176+RLAlpha49@users.noreply.github.com>
2024-08-28 22:50:59 -0500

    Remove footer

--- AMU-Rewrite/GUI/MainWindow.xaml.cs
+++ AMU-Rewrite/GUI/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace AMU_Rewrite.GUI
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string name = "")
+        private void OnPropertyChanged([CallerMemberName] string name = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
